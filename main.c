074cab41a0f796775477c09d0c75ef0347959f75
Manuel Wetzel <manuel.wetzel@hpe.com>
2018-02-02 11:07:55 +0100

    Double link list is working and deletable

--- main.c
+++ main.c
@@ -1,64 +1,84 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int nummer =0;
+struct person_t* head;
 
 struct person_t{
         char name[50+1];
         float height;
         int age;
+        struct person_t* next;
+        struct person_t* prev;
     };
 
 
-void add(struct person_t* ptr){
-    ptr += nummer;
-    nummer++;
+void add(){
+    struct person_t* ptr = head;
+    struct person_t* person = (struct person_t*)malloc(sizeof(struct person_t));
+    if(head == NULL) {
+        head = person;
+        head->next = NULL;
+    }
+    else {
+        while(ptr->next != NULL) ptr = ptr->next;
+        ptr->next = person;
+        person->prev = ptr;
+    }
+
     char txt[50+1];
     float gr;
     int age;
     printf("Bitte einen Namen eingeben:");
     scanf("%s", &txt);
-    printf("Gr��e:");
+    printf("Größe:");
     scanf("%f", &gr);
     printf("Alter:");
     scanf("%d", &age);
-    strcpy(ptr->name, txt);
-    ptr -> height = gr;
-    ptr -> age = age;
+    strcpy(person->name, txt);
+    person->height = gr;
+    person->age = age;
 }
 
-void print(struct person_t* ptr){
-    for(int i=0;i<nummer;i++){
-        printf("%s ist %f gro� und %d alt.\n",ptr->name,ptr->height,ptr->age);
-        ptr++;
-    }
+void print(){
+    struct person_t* ptr = head;
+	while(ptr != NULL) {
+		printf("%s ist %f groß und %d alt.\n",ptr->name,ptr->height,ptr->age);
+		ptr = ptr->next;
+	}
+	printf("\n");
+
 }
 
-void deleteP(struct person_t* ptr){
-    nummer =0;
-    for(int i=0;i<nummer;i++){
-        strcpy(ptr->name, "");
-        ptr->height = 0;
-        ptr->age = 0;
-        ptr++;
+void deleteAll(){
+    struct person_t* ptr = head;
+    struct person_t* temp;
+    while(ptr != NULL){
+        temp = ptr;
+        free(ptr);
+        ptr = temp->next;
     }
+    head = NULL;
 }
 
 
 int main(){
 
-    struct person_t person[100];
     int auswahl=0;
     while(auswahl!=4){
-        printf("Was m�chten Sie tun?\n (1) Add Person\n (2) Print all\n (3) Delete Person\n (4) End Programm\n");
+        printf("Was möchten Sie tun?\n (1) Add Person\n (2) Print all\n (3) Delete Everything\n (4) End Programm\n");
         scanf("%d", &auswahl);
         switch(auswahl){
-        case 1: add(person);
+        case 1: add();
             break;
-        case 2: print(person);
+        case 2: print();
             break;
-        case 3: deleteP(person);
+        case 3: deleteAll();
             break;
+        case 4:
+            break;
+        default:
+            printf("Ungültige Eingabe!\n");
         }
     }
 return 0;
