df09ca203b83b23a8cf20d10c6b0dbe99b4cef92
ManuelWe <manuel.wetzel@hpe.com>
2018-02-08 14:38:36 +0100

    Code Sortiert, encoding angepasst

--- main.c
+++ main.c
@@ -2,43 +2,38 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct person_t* head;
+struct person_t *head;
 int counter = 0;
 
-struct person_t
-{
-    char name[50+1];
+struct person_t {
+    char name[50 + 1];
     float height;
     int age;
-    struct person_t* next;
-    struct person_t* prev;
+    struct person_t *next;
+    struct person_t *prev;
 };
 
 
-void add()
-{
-    struct person_t* ptr = head;
-    struct person_t* person = (struct person_t*)malloc(sizeof(struct person_t));
-    if(head == NULL)
-    {
+void add() {
+    struct person_t *ptr = head;
+    struct person_t *person = (struct person_t *) malloc(sizeof(struct person_t));
+    if (head == NULL) {
         head = person;
         head->next = NULL;
         head->prev = NULL;
-    }
-    else
-    {
-        while(ptr->next != NULL) ptr = ptr->next;
+    } else {
+        while (ptr->next != NULL) ptr = ptr->next;
         ptr->next = person;
         person->next = NULL;
         person->prev = ptr;
     }
 
-    char txt[50+1];
+    char txt[50 + 1];
     float gr;
     int age;
     printf("Bitte einen Namen eingeben:");
     scanf("%s", &txt);
-    printf("Größe:");
+    printf("Groesse:");
     scanf("%f", &gr);
     printf("Alter:");
     scanf("%d", &age);
@@ -47,29 +42,24 @@ void add()
     person->age = age;
 }
 
-void testPersons()
-{
+void testPersons() {
     counter++;
-    struct person_t* ptr = head;
-    struct person_t* person = (struct person_t*)malloc(sizeof(struct person_t));
-    if(head == NULL)
-    {
+    struct person_t *ptr = head;
+    struct person_t *person = (struct person_t *) malloc(sizeof(struct person_t));
+    if (head == NULL) {
         head = person;
         head->next = NULL;
         head->prev = NULL;
-    }
-    else
-    {
-        while(ptr->next != NULL)
-        {
+    } else {
+        while (ptr->next != NULL) {
             ptr = ptr->next;
         }
         ptr->next = person;
         person->next = NULL;
         person->prev = ptr;
     }
-    char txt[50+1] = "test";
-    char value[10+1];
+    char txt[50 + 1] = "test";
+    char value[10 + 1];
     sprintf(value, "%d", counter);
     strcat(txt, value);
     float gr = counter;
@@ -79,24 +69,20 @@ void testPersons()
     person->age = age;
 }
 
-void print()
-{
-    struct person_t* ptr = head;
-    while(ptr != NULL)
-    {
-        printf("%s ist %f groß und %d alt.\n",ptr->name,ptr->height,ptr->age);
+void print() {
+    struct person_t *ptr = head;
+    while (ptr != NULL) {
+        printf("%s ist %f gross und %d alt.\n", ptr->name, ptr->height, ptr->age);
         ptr = ptr->next;
     }
     printf("\n");
 
 }
 
-void deleteAll()
-{
-    struct person_t* ptr = head;
-    struct person_t* temp;
-    while(ptr != NULL)
-    {
+void deleteAll() {
+    struct person_t *ptr = head;
+    struct person_t *temp;
+    while (ptr != NULL) {
         temp = ptr;
         free(ptr);
         ptr = temp->next;
@@ -104,37 +90,24 @@ void deleteAll()
     head = NULL;
 }
 
-void deletePerson(char *name)
-{
-    struct person_t* ptr = head;
+void deletePerson(char *name) {
+    struct person_t *ptr = head;
     int deleteCounter = 0;
-    do
-    {
-        if(strcmp(ptr->name, name) == 0)
-        {
-            if(ptr->next != NULL)
-            {
+    do {
+        if (strcmp(ptr->name, name) == 0) {
+            if (ptr->next != NULL) {
                 ptr->next->prev = ptr->prev;
-            }
-            else if(ptr != head)
-            {
+            } else if (ptr != head) {
                 ptr->prev->next = NULL;
-            }
-            else
-            {
+            } else {
                 head = NULL;
             }
 
-            if(ptr->prev != NULL)
-            {
+            if (ptr->prev != NULL) {
                 ptr->prev->next = ptr->next;
-            }
-            else if(ptr != head && head != NULL)
-            {
+            } else if (ptr != head && head != NULL) {
                 ptr->next->prev = NULL;
-            }
-            else
-            {
+            } else {
                 head = ptr->next;
             }
 
@@ -142,56 +115,48 @@ void deletePerson(char *name)
             deleteCounter++;
         }
         ptr = ptr->next;
-    }
-    while(ptr != NULL);
-    if(deleteCounter==0)
-    {
+    } while (ptr != NULL);
+    if (deleteCounter == 0) {
         printf("There's no Person with name %s!\n", name);
-    }
-    else
-    {
+    } else {
         printf("%d Element(s) deleted!\n", deleteCounter);
     }
 
 }
 
 
-int main()
-{
+int main() {
 
-    int auswahl=0;
-    char name[20+1];
+    int auswahl = 0;
+    char name[20 + 1];
 
-    while(auswahl!=5)
-    {
-        printf("Was möchten Sie tun?\n (1) Add Person\n (2) Print all\n (3) Delete Everything\n (4) Delete Person by Name\n (5) End Programm\n (6) Add Group of test Persons\n");
+    while (auswahl != 5) {
+        printf("Was moechten Sie tun?\n (1) Add Person\n (2) Print all\n (3) Delete Everything\n (4) Delete Person by Name\n (5) End Programm\n (6) Add Group of test Persons\n");
         scanf("%d", &auswahl);
-        switch(auswahl)
-        {
-        case 1:
-            add();
-            break;
-        case 2:
-            print();
-            break;
-        case 3:
-            deleteAll();
-            break;
-        case 4:
-            printf("Bitte geben Sie den Namen ein:");
-            scanf("%s", &name);
-            deletePerson(name);
-            break;
-        case 5:
-            break;
-        case 6:
-            for (int i=0; i<8; i++)
-            {
-                testPersons();
-            }
-            break;
-        default:
-            printf("Ungültige Eingabe!\n");
+        switch (auswahl) {
+            case 1:
+                add();
+                break;
+            case 2:
+                print();
+                break;
+            case 3:
+                deleteAll();
+                break;
+            case 4:
+                printf("Bitte geben Sie den Namen ein:");
+                scanf("%s", &name);
+                deletePerson(name);
+                break;
+            case 5:
+                break;
+            case 6:
+                for (int i = 0; i < 8; i++) {
+                    testPersons();
+                }
+                break;
+            default:
+                printf("Ungueltige Eingabe!\n");
         }
     }
     return 0;
